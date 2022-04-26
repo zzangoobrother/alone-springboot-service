151a96e780224c57da2b3b2402f368323f4ada19
zzangoobrother <com1368@naver.com>
2022-04-26 11:18:30 +0900

    기본 세팅

---
+++ src/main/java/com/alone/springboot/alonespringbootservice/AloneSpringbootServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alone.springboot.alonespringbootservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AloneSpringbootServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AloneSpringbootServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/alone/springboot/alonespringbootservice/AloneSpringbootServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alone.springboot.alonespringbootservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AloneSpringbootServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
