f4c09be6a74a47a927c787a8707bbe52856cd0cf
zzangoobrother <com1368@naver.com>
2022-04-27 18:17:25 +0900

    게시글 전체 조회 화면 구현

--- src/main/java/com/alone/springboot/alonespringbootservice/service/posts/PostsService.java
+++ src/main/java/com/alone/springboot/alonespringbootservice/service/posts/PostsService.java
@@ -2,9 +2,12 @@ package com.alone.springboot.alonespringbootservice.service.posts;
 
 import com.alone.springboot.alonespringbootservice.domain.posts.Posts;
 import com.alone.springboot.alonespringbootservice.domain.posts.PostsRepository;
+import com.alone.springboot.alonespringbootservice.web.dto.PostsListResponseDto;
 import com.alone.springboot.alonespringbootservice.web.dto.PostsResponseDto;
 import com.alone.springboot.alonespringbootservice.web.dto.PostsSaveRequestDto;
 import com.alone.springboot.alonespringbootservice.web.dto.PostsUpdateRequestDto;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -35,5 +38,12 @@ public class PostsService {
         .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
     return new PostsResponseDto(entity);
+  }
+
+  @Transactional(readOnly = true)
+  public List<PostsListResponseDto> findAllDesc() {
+    return postsRepository.findAllDesc().stream()
+        .map(PostsListResponseDto::new)
+        .collect(Collectors.toList());
   }
 }
