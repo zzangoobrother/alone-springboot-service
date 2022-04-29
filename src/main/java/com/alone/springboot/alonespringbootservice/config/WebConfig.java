036a73e5560361dcaa562e0b1c6a6cd81f2a8fe7
zzangoobrother <com1368@naver.com>
2022-04-29 09:36:26 +0900

    로그인 정보 톰캣 내장 세션 저장 구현

---
+++ src/main/java/com/alone/springboot/alonespringbootservice/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.alone.springboot.alonespringbootservice.config;
+
+import com.alone.springboot.alonespringbootservice.config.auth.LoginUserArgumentResolver;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+    resolvers.add(loginUserArgumentResolver);
+  }
+}
--- src/main/java/com/alone/springboot/alonespringbootservice/web/IndexController.java
+++ src/main/java/com/alone/springboot/alonespringbootservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.alone.springboot.alonespringbootservice.web;
 
+import com.alone.springboot.alonespringbootservice.config.auth.LoginUser;
 import com.alone.springboot.alonespringbootservice.config.auth.dto.SessionUser;
 import com.alone.springboot.alonespringbootservice.service.posts.PostsService;
 import com.alone.springboot.alonespringbootservice.web.dto.PostsResponseDto;
@@ -15,13 +16,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
   private final PostsService postsService;
-  private final HttpSession httpSession;
 
   @GetMapping("/")
-  public String index(Model model) {
+  public String index(Model model, @LoginUser SessionUser user) {
     model.addAttribute("posts", postsService.findAllDesc());
-
-    SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
     if (user != null) {
       model.addAttribute("userName", user.getName());
