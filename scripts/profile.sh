277c5c5f62925c0e4910b87b02526fecb4cab285
zzangoobrother <com1368@naver.com>
2022-05-04 21:03:13 +0900

    스크립트 오류 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -26,7 +26,7 @@ function find_idle_profile() {
 function find_idle_port() {
     IDLE_PROFILE=$(find_idle_profile)
 
-    if [ ${IDE_PROFILE} == real1 ]
+    if [ ${IDLE_PROFILE} == real1 ]
     then
       echo "8081"
     else
